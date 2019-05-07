1ed0c4071525ae338ee0832d1d11bc3620595140
Bartosz Taczała <bartosz.taczala@siili.com>
2019-05-07 11:59:09 +0200

    Provide a selector option
    This allows to switch between using dmenu/rofi

--- selector.go
+++ selector.go
@@ -7,13 +7,13 @@ import (
 	"strings"
 )
 
-func selector(history []string, max int) error {
+func selector(history []string, max int, tool string) error {
 	// reverse the history
 	for i, j := 0, len(history)-1; i < j; i, j = i+1, j-1 {
 		history[i], history[j] = history[j], history[i]
 	}
 
-	selected, err := dmenu(history, max)
+	selected, err := dmenu(history, max, tool)
 	if err != nil {
 		// dmenu exits with error when no selection done
 		return nil
@@ -25,16 +25,23 @@ func selector(history []string, max int) error {
 	return err
 }
 
-func dmenu(list []string, max int) (string, error) {
+func dmenu(list []string, max int, tool string) (string, error) {
 	if len(list) == 0 {
 		return "", nil
 	}
 
-	args := []string{"dmenu", "-b",
-		"-fn",
-		"-misc-dejavu sans mono-medium-r-normal--17-120-100-100-m-0-iso8859-16",
-		"-l",
-		strconv.Itoa(max)}
+    var args []string;
+    if tool == "dmenu" {
+        args = []string{"dmenu", "-b",
+            "-fn",
+            "-misc-dejavu sans mono-medium-r-normal--17-120-100-100-m-0-iso8859-16",
+            "-l",
+            strconv.Itoa(max)}
+    } else {
+        args = []string{"rofi", "-dmenu",
+            "-lines",
+            strconv.Itoa(max)}
+    }
 
 	// dmenu will break if items contain newlines, so we must pass them as literals.
 	// however, when it sends them back, we need a way to restore them to non literals
@@ -55,9 +62,10 @@ func dmenu(list []string, max int) (string, error) {
 
 	input := strings.NewReader(strings.Join(reprList, "\n"))
 
-	cmd := exec.Cmd{Path: "/usr/bin/dmenu", Args: args, Stdin: input}
+	cmd := exec.Cmd{Path: "/usr/bin/" + tool, Args: args, Stdin: input}
 	selected, err := cmd.Output()
 	if err != nil {
+        fmt.Printf("%s", err)
 		return "", err
 	}
 	trimmed := selected[:len(selected)-1] // drop newline
